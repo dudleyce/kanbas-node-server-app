6998807bfe38e4271988e8bffc46e01e0fe2dd93
dudleyce <dudley.c@northeastern.edu>
2024-04-04 13:51:56 -0400

    assignments routes fix

--- Kanbas/assignments/routes.js
+++ Kanbas/assignments/routes.js
@@ -3,10 +3,10 @@ import Database from "../Database/index.js";
 export default function AssignmentRoutes(app) {
 
 
-    app.get("/api/assignments/:id", (req, res) => {
-        const { id } = req.params;
+    app.get("/api/assignments/:assignmentId", (req, res) => {
+        const { assignmentId } = req.params;
         const assignment = Database.assignments
-          .find((a) => a._id === id);
+          .find((a) => a._id === assignmentId);
         if (!assignment) {
           res.status(404).send("Assignment not found");
           return;
@@ -14,31 +14,34 @@ export default function AssignmentRoutes(app) {
         res.send(assignment);
       });
     
-    app.put("/api/assignments/:id", (req, res) => {
-        const { id } = req.params;
+    app.put("/api/assignments/:assignmentId", (req, res) => {
+        const { assignmentId } = req.params;
         const assignment = req.body;
         Database.assignments = Database.assignments.map((a) =>
-          a._id === id ? { ...a, ...assignment } : a
+          a._id === assignmentId ? { ...a, ...assignment } : a
         );
         res.sendStatus(204);
       });
     
-    app.delete("/api/assignments/:id", (req, res) => {
-        const { id } = req.params;
+    app.delete("/api/assignments/:assignmentId", (req, res) => {
+        const { assignmentId } = req.params;
         Database.assignments = Database.assignments
-          .filter((a) => a._id !== id);
+          .filter((a) => a._id !== assignmentId);
         res.sendStatus(204);
       });
     
-    app.post("/api/assignments", (req, res) => {
+    app.post("/api/courses/:courseId/assignments", (req, res) => {
+      const {courseId} = req.params;
         const assignment = { ...req.body,
+          course: courseId,
           _id: new Date().getTime().toString() };
         Database.assignments.push(assignment);
         res.send(assignment);
       });
     
-  app.get("/api/assignments", (req, res) => {
-    const assignments = Database.assignments;
+  app.get("/api/courses/:courseId/assignments", (req, res) => {
+    const {courseId} = req.params;
+    const assignments = Database.assignments.filter((a) => a.course === courseId);
     res.send(assignments);
   });
 }
